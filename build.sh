de57ba3894a3fb30a641d6d5669345f810668385
Itamar Hassin <ihassin@mac.com>
2022-04-17 08:33:25 -0400

    Edits

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/zsh
 bundle exec just-the-docs rake search:init
 bundle exec jekyll build
+git add .
+git commit -am"Edits"
+git push
+
+aws s3 cp ./_site s3://inceptions-info-customresourcestack-s3bucketroot-1fb8ssmgnkr8f --recursive --region us-east-1 --profile personal
+
