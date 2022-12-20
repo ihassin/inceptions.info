8c433d4c7caefb7bfdabff27d149d403dc6f41d2
Itamar Hassin <ihassin@gmail.com>
2022-12-20 11:03:01 -0500

    Edits

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ git add .
 git commit -am"Edits"
 git push
 
-aws s3 cp ./_site s3://inceptions-customresourcestack-1n9j0-s3bucketroot-ouce98s0f9on --recursive --region us-east-1 --profile personal
+aws s3 cp ./_site s3://inceptions.in-context.com --recursive --region us-east-1 --profile personal
 
